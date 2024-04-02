3663c3e2cd47ba84a79bfca238a713af5a84ca24
emilianogastaldo <emiliano0303@gmail.com>
2024-04-02 15:35:16 +0200

    creato l'apploader

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
+import AppLoader from './components/AppLoader.vue';
 
 const endpoint = 'http://127.0.0.1:8000/api/projects';
 export default {
   name: 'App',
   components: { AppHeader, ProjectsList },
-  data: () => ({ projects: [] }),
+  data: () => ({ projects: [], isLoading: false }),
   methods: {
     fetchProject() {
+      this.isLoading = true;
       axios.get(endpoint)
         .then(res => {
           this.projects = res.data;
@@ -17,7 +19,9 @@ export default {
         .catch(err => {
           console.error(err);
         })
-        .then()
+        .then(() => {
+          this.isLoading = false;
+        })
     }
   },
   created() {
@@ -28,7 +32,8 @@ export default {
 
 <template>
   <AppHeader />
-  <main>
+  <AppLoader v-if="isLoading" />
+  <main v-else>
     <div class="container mt-5">
       <h1>Progetti</h1>
       <ProjectsList :projects="projects" />
---
+++ src/components/AppLoader.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'AppLoader',
+
+};
+</script>
+
+<template>
+    <div class="loader-overlay">
+        <div class="spinner-border" role="status">
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.loader-overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1;
+
+    .spinner-border {
+        width: 100px;
+        height: 100px;
+    }
+}
+</style>

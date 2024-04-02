b38e2a4c1b562a7b45fccef25c93febd3c666867
emilianogastaldo <emiliano0303@gmail.com>
2024-04-02 18:00:34 +0200

    creata la 404 page

---
+++ src/components/pages/NotFoundPage.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'NotFoundPage',
+}
+</script>
+<template>
+    <div class="overlay d-flex align-items-center justify-content-center">
+        <h3>404 | Page not found</h3>
+    </div>
+</template>
+<style>
+.overlay {
+    height: 70vh;
+}
+</style>

a787b9954595f1ba6a805b9d51a18fb20bb9ff9f
emilianogastaldo <emiliano0303@gmail.com>
2024-04-02 18:10:10 +0200

    aggiunti bottoni per toranre indietro

--- src/components/pages/NotFoundPage.vue
+++ src/components/pages/NotFoundPage.vue
@@ -4,8 +4,10 @@ export default {
 }
 </script>
 <template>
-    <div class="overlay d-flex align-items-center justify-content-center">
+    <div class="overlay flex-column d-flex align-items-center justify-content-center">
         <h3>404 | Page not found</h3>
+        <button class="btn btn-outline-secondary mt-5" @click="$router.back()">Torna indietro</button>
+        <RouterLink class="btn btn-outline-primary mt-5" to="/">Torna alla home</RouterLink>
     </div>
 </template>
 <style>

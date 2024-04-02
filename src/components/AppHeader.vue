c268e0c1f368871b1a3041122e22b67ccef9c75b
emilianogastaldo <emiliano0303@gmail.com>
2024-04-02 18:00:51 +0200

    sistemata la navbar con i RouterLink

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,9 +6,23 @@ export default {
 
 <template>
     <header>
-        <nav class="navbar bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container">
-                <a class="navbar-brand" href="#">Boolfolio</a>
+                <RouterLink class="navbar-brand" to="/">Boolfolio</RouterLink>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <RouterLink class="nav-link" to="/">Home</RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link" to="/contact-us">Contattaci</RouterLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     </header>

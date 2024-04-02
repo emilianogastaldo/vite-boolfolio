202555e5cec637873faa5a876a204ea76505ef96
emilianogastaldo <emiliano0303@gmail.com>
2024-04-02 15:21:23 +0200

    aggiunte computed alla card

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -2,6 +2,26 @@
 export default {
     name: 'ProjectCard',
     props: { project: Object },
+    computed: {
+        abstract() {
+            return this.project.content.slice(0, 400) + '...';
+        },
+        publicationDate() {
+            const date = new Date(this.project.created_at);
+
+            let day = date.getDate();
+            let month = date.getMonth() + 1;
+            const year = date.getFullYear();
+            const hours = date.getHours();
+            const minutes = date.getMinutes();
+            const seconds = date.getSeconds();
+
+            if (day < 10) day = '0' + day;
+            if (month < 10) month = '0' + month;
+
+            return `${day}/${month}/${year} alle ${hours}:${minutes}`;
+        }
+    }
 }
 </script>
 
@@ -20,7 +40,10 @@ export default {
                     {{ tech.label }}
                 </span>
             </h6>
-            <p class="card-text" v-text="project.content"></p>
+            <p class="card-text">{{ abstract }}</p>
+        </div>
+        <div class="card-footer text-end">
+            <div>Creato il {{ publicationDate }}</div>
         </div>
     </div>
 </template>
